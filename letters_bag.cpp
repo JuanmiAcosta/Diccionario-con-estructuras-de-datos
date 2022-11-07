e93fc0d710b17f42ed3b13d68608c045c4c3a233
Luisotorres3 <74504901+Luisotorres3@users.noreply.github.com>
2022-11-07 10:56:49 +0100

    Update letters_bag.cpp

--- letters_bag.cpp
+++ letters_bag.cpp
@@ -1,1 +1,34 @@
 #include "letters_bag.h"
+
+
+Bag<T>::Bag() {
+
+}
+
+Bag<T>::Bag(const Bag<T> &other) {
+
+}
+
+void Bag<T>::add(const T &element) {
+
+}
+
+T Bag<T>::get() {
+
+}
+
+void Bag<T>::clear() {
+
+}
+
+unsigned int Bag<T>::size() const {
+
+}
+
+bool Bag<T>::empty() {
+
+}
+
+const Bag<T> Bag<T>::operator=(const Bag<T> &cl) {
+
+}
